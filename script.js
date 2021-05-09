6ef784af18ac6b15ad2c541c6b87edeabcec7283
Tim Groenewoud <74865860+TimGroenewoud@users.noreply.github.com>
2021-05-09 21:17:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Acadmey Studenten")
+let vermenigvuldigen= 1230941*1823794;
+/*  
+// "Dit is een grote som"
+console.log (vermenigvuldigen);
+let delen= 637263 / 54;
+console.log (delen);
+*/
